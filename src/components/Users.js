e6ad7af1badd829bf8a9e858331bf779a0ae00a5
sanchia-menezes <sanchia.menezes19@gmail.com>
2023-01-27 17:40:51 -0500

    Next set of functionality

---
+++ src/components/Users.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Outlet, Link, useSearchParams } from 'react-router-dom';
+
+const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get('filter') === 'active';
+  return (
+    <div>
+      <Link to='1'>User 1</Link>
+      <Link to='2'>User 2</Link>
+      <Link to='3'>User 3</Link>
+      <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: 'active' })}>
+          Active Users
+        </button>
+        <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      </div>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
+    </div>
+  );
+};
+
+export default Users;
