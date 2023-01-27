985e54f574603f3ec34bd86b90f087701ec76c84
sanchia-menezes <sanchia.menezes19@gmail.com>
2023-01-27 11:31:02 -0500

    Navlink, Link and useNavigate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import OrderSummary from './components/OrderSummary';
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/about' element={<About />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='order-summary' element={<OrderSummary />}></Route>
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Home</div>
+      <button onClick={() => navigate('order-summary')}>Place order</button>
+    </>
+  );
 };
 
 export default Home;
